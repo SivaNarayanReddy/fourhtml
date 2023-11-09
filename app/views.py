0e9a2e9eeb226320239b522fe854e85e8193a561
sivareddy01 <sivanarayanakummitha@gmail.com>
2023-11-09 19:06:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+
+def pandhi(request):
+    
+    return render (request,'one.html')
+
+def oyo(request):
+
+    return render(request,'two.html')
+
+def instagram(request):
+
+    return render(request,'three.html')
+
+def room(request):
+
+    return render(request,'four.html')
+
